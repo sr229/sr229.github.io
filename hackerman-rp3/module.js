ff85733d0804b8d54aa3197b266b641ad6e2538d
Ayase Minori <chinodesuuu@gmail.com>
2026-03-20 07:26:42 +0000

    attempt to fix rp3 version
    Signed-off-by: GitHub <noreply@github.com>

--- hackerman-rp3/module.js
+++ hackerman-rp3/module.js
@@ -1,17 +1,13 @@
 if (typeof Module === 'undefined') {
     Module = {};
 }
+Module['mainScriptUrlOrBlob'] = location.origin + "/out.js";
 Module['arguments'] = [
     '-nic', 'none',
     '-M', 'raspi3ap', '-nographic',
-    '-m', '512M',
-    '-accel', 'tcg,tb-size=500', '-smp', '4',
+    '-m', '512M', '-accel', 'tcg,tb-size=500,thread=multi', '-smp', '4',
     '-dtb', '/pack/bcm2710-rpi-3-b-plus.dtb',
     '-kernel', '/pack/kernel8.img',
     '-drive', 'file=/pack/rootfs.bin,format=raw,if=sd',
-    '-append', 'earlycon=pl011,0x3f201000 console=ttyAMA0,115200 loglevel=6 initcall_blacklist=bcm2835_pm_driver_init root=/dev/mmcblk0 rootfstype=ext4 rootwait no_console_suspend'
+    '-append', 'earlycon=pl011,0x3f201000 console=ttyAMA0,115200 loglevel=8 initcall_blacklist=bcm2835_pm_driver_init root=/dev/mmcblk0 rootfstype=ext4 rootwait no_console_suspend'
 ];
-Module['locateFile'] = function(path, prefix) {
-    return '/hackerman-rp3/' + path;
-};
-Module['mainScriptUrlOrBlob'] = '/hackerman-rp3/out.js'
